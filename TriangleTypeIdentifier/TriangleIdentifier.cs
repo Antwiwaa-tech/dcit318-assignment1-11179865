1ddaa63b3cbcc7182936af687d4a0e749679c782
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-31 16:27:39 +0000

    triangle dentifier

---
+++ TriangleTypeIdentifier/TriangleIdentifier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Triangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's determine the triangle type");
+
+            Console.Write("Side 1: ");
+            int side1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Side 2: ");
+            int side2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Side 3: ");
+            int side3 = int.Parse(Console.ReadLine());
+
+            // Check if valid triangle first
+            if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+            {
+                if (side1 == side2 && side2 == side3)
+                {
+                    Console.WriteLine("The triangle is Equilateral.");
+                }
+                else if (side1 == side2 || side1 == side3 || side2 == side3)
+                {
+                    Console.WriteLine("The triangle is Isosceles.");
+                }
+                else
+                {
+                    Console.WriteLine("The triangle is Scalene.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The values do not form a valid triangle.");
+            }
+        }
+    }
+}
