2e05f75377b7bbf1e84ea8bc88e839ba0e7ef132
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-31 16:05:03 +0000

    ticket price calculator

---
+++ TicketPriceCalculator/TicketPriceCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace TicketPriceCalculatorApp
+{
+    class TicketPriceClac
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Get Your Tickets Here\nEnter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.ReadLine();
+
+            int ticketPrice = 10;
+
+            if (age <= 12 || age >= 65 )
+            {
+                Console.WriteLine("You get a discount\nYour price is 7 cedis");
+            }
+            else
+            {
+                Console.WriteLine($"Movie ticket is {ticketPrice} cedis");
+            }
+        }
+    }
+}
